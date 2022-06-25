7e302c611ce9b0c28d5ba51d5721ef94528f74e0
kushi4261 <108167860+kushi4261@users.noreply.github.com>
2022-06-25 19:20:53 +0530

    Update MapsActivity.java

--- exp8/MapsActivity.java
+++ exp8/MapsActivity.java
@@ -1,1 +1,106 @@
-x
+package com.example.program8;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.program8.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private TextView tv;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Bengaluru and move the camera
+        LatLng Bengaluru = new LatLng(13, 78);
+        mMap.addMarker(new MarkerOptions().position(Bengaluru).title("Marker in Bengaluru"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Bengaluru));
+    }
+
+    public void setmMap(GoogleMap mMap) {
+        this.mMap = mMap;
+    }
+
+    public void onSearch(View view) {
+        List<Address> addressList = null;
+        EditText et_location = (EditText) findViewById(R.id.et1);
+        String location = et_location.getText().toString();
+        if (location != null || location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+
+            try {
+
+
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
+    public void onType(View view) {
+        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
+            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        }
+    }
+
+    public void onZoom(View view) {
+        if (view.getId() == R.id.zoomin) {
+            mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        }
+        if (view.getId() == R.id.zoomout) {
+            mMap.animateCamera(CameraUpdateFactory.zoomOut());
+        }
+
+    }
+}
+
