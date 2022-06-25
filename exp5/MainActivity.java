4259db57d663eb3525e42e86a3b3f271e0a6d116
kushi4261 <108167860+kushi4261@users.noreply.github.com>
2022-06-25 19:13:59 +0530

    Update MainActivity.java

--- exp5/MainActivity.java
+++ exp5/MainActivity.java
@@ -1,1 +1,68 @@
-zz
+package com.example.smsemailphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    Button sendBtn;
+    Button btnSendEmail;
+    Button btnPhone;
+    String phoneNo;
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendBtn = (Button) findViewById(R.id.btnSendSMS);
+        btnSendEmail = (Button) findViewById(R.id.btnSendEmail);
+        btnPhone = (Button) findViewById(R.id.btnDialPhone);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendSMSMessage();
+
+            }
+});
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+        btnPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhoneDial();
+            }
+        });
+    }
+    protected void sendEmail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ba.mohan@gmail.com"});
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "subject Test");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Message Body Test");
+        startActivity(emailIntent);
+    }
+    protected void sendSMSMessage() {
+        Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+        sendIntent.putExtra("sms_body", "default content");
+        sendIntent.setType("vnd.android-dir/mms-sms");
+        startActivity(sendIntent);
+        Toast.makeText(getApplicationContext(), "SMS sent.",
+                Toast.LENGTH_LONG).show();
+    }
+    protected void PhoneDial() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent); }
+}
+
