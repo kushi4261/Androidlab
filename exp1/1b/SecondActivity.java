721ac853d31cfca2580f6e8bbe217845c7272f1f
kushi4261 <108167860+kushi4261@users.noreply.github.com>
2022-06-25 10:49:29 +0530

    Update SecondActivity.java

--- exp1/1b/SecondActivity.java
+++ exp1/1b/SecondActivity.java
@@ -1,1 +1,31 @@
-yh
+package com.example.exp1;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        //actionbar title
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Second Activity");
+        //intent to get data
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("NAME");
+        String email = intent.getStringExtra("EMAIL");
+        String phone = intent.getStringExtra("PHONE");
+
+        //Textview
+        TextView mresult = findViewById(R.id.result);
+        //setText
+        mresult.setText("Name: "+name+ "\nEmail: "+email+ "\nPhone:"+phone);
+    }
+}
+
