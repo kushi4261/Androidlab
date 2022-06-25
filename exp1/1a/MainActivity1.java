58df3fc6d377fe9448bb291dcaabbc2eac71be5d
kushi4261 <108167860+kushi4261@users.noreply.github.com>
2022-06-25 10:39:53 +0530

    Update MainActivity1.java

--- exp1/1a/MainActivity1.java
+++ exp1/1a/MainActivity1.java
@@ -1,1 +1,30 @@
-n
+package com.example.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "DemoApplication";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.btnDoMagic);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG,"This is a magic log message!");
+                Toast.makeText(getApplicationContext(), "It's magic", Toast.LENGTH_SHORT)
+                        .show();
+
+            }
+        });
+    }
+}
+
