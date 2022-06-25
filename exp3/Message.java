240841e95a9daec643d7e8b9b7dc621b363e6c0a
kushi4261 <108167860+kushi4261@users.noreply.github.com>
2022-06-25 19:10:07 +0530

    Update Message.java

--- exp3/Message.java
+++ exp3/Message.java
@@ -1,1 +1,11 @@
-dd
+package com.example.program3;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Message {
+    public static void message(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
+
